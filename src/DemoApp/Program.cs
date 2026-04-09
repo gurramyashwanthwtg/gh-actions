464cbcfca7f7d04d4d7536c36fa416614f5e7422
gurramyashwanthwtg <gurram.yashwanth@wisetechglobal.com>
2026-04-09 15:30:14 +0530

    fix: add Subtract method to resolve build error

--- src/DemoApp/Program.cs
+++ src/DemoApp/Program.cs
@@ -8,5 +8,7 @@ namespace DemoApp
     public static class MathOps
     {
         public static int Add(int a, int b) => a + b;
+
+        public static int Subtract(int a, int b) => a - b;
     }
 }
