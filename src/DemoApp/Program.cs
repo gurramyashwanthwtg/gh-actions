a11f9265abc3ff66f178a9fc65d6f31df4aebdbc
gurramyashwanthwtg <gurram.yashwanth@wisetechglobal.com>
2026-04-09 17:42:44 +0530

    feat: add multiply operation and test

--- src/DemoApp/Program.cs
+++ src/DemoApp/Program.cs
@@ -2,6 +2,7 @@ using DemoApp;
 
 Console.WriteLine($"2 + 3 = {MathOps.Add(2, 3)}");
 Console.WriteLine($"10 - 3 = {MathOps.Subtract(10, 3)}");
+Console.WriteLine($"4 * 5 = {MathOps.Multiply(4, 5)}");
 
 namespace DemoApp
 {
@@ -10,5 +11,7 @@ namespace DemoApp
         public static int Add(int a, int b) => a + b;
 
         public static int Subtract(int a, int b) => a - b;
+
+        public static int Multiply(int a, int b) => a * b;
     }
 }
